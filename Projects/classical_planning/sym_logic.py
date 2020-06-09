a57a688be3b5063dd12a03d48c4877e9694579b6
Piyush Gupta <piyushg91@gmail.com>
2020-06-08 21:44:05 -0700

    Defaulting to just 2 inputs for now and lowercase

--- Projects/classical_planning/sym_logic.py
+++ Projects/classical_planning/sym_logic.py
@@ -3,8 +3,8 @@ from typing import List
 
 
 class SymLogicParser(object):
-    def __init__(self, symbols: List[str]):
-        self.syms = symbols
+    def __init__(self):
+        self.syms = ['p', 'q']
         possibilities = [True, False]
         self.pos = set(itertools.product(possibilities, repeat=len(self.syms)))
 
--- Projects/classical_planning/sym_logic_test.py
+++ Projects/classical_planning/sym_logic_test.py
@@ -4,29 +4,29 @@ from sym_logic import SymLogicParser
 
 class SymLogicTest(TestCase):
     def setUp(self):
-        self.parser1 = SymLogicParser(['P'])
-        self.parser2 = SymLogicParser(['P', 'Q'])
-        self.parser3 = SymLogicParser(['A', 'B', 'C'])
+        self.parser1 = SymLogicParser()
+        self.parser2 = SymLogicParser()
+        # self.parser3 = SymLogicParser(['A', 'B', 'C'])
 
     def test__init__(self):
         correct1 = {(True,), (False,)}
         correct2 = {(True, True), (True, False), (False, True), (False, False)}
-        correct3 = {(True, True, True), (True, True, False), (True, False, True), (True, False, False),
-                    (False, True, True), (False, True, False), (False, False, True), (False, False, False)}
+        # correct3 = {(True, True, True), (True, True, False), (True, False, True), (True, False, False),
+        #             (False, True, True), (False, True, False), (False, False, True), (False, False, False)}
         self.assertEqual(correct1, self.parser1.pos)
         self.assertEqual(correct2, self.parser2.pos)
-        self.assertEqual(correct3, self.parser3.pos)
+        # self.assertEqual(correct3, self.parser3.pos)
 
     def test_not_p(self):
-        inp = '~P'
-        correct = {{'P': True}: {False},
-                   {'P': False}: {True}}
+        inp = '~p'
+        correct = {{'p': True}: {False},
+                   {'p': False}: {True}}
         seen = self.parser1.create_column(inp)
         self.assertEqual(correct, seen)
 
     def test_and(self):
-        inp = 'P ^ Q'
-        p, q = 'P', 'Q'
+        inp = 'p ^ q'
+        p, q = 'p', 'q'
         correct = {{p: True, q: True}: True,
                    {p: False, q: True}: False,
                    {p: True, q: False}: False,
@@ -35,8 +35,8 @@ class SymLogicTest(TestCase):
         self.assertEqual(correct, seen)
 
     def test3(self):
-        inp = '~(~P V ~Q)'
-        p, q = 'P', 'Q'
+        inp = '~(~p V ~q)'
+        p, q = 'p', 'q'
         correct = {{p: True, q: True}: True,
                    {p: False, q: True}: False,
                    {p: True, q: False}: False,
