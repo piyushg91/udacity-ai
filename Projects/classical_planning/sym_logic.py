8bb4fb4d7da51e9f995f6d7c70c1babc1f7db264
Piyush Gupta <piyushg91@gmail.com>
2020-06-08 23:45:03 -0700

    Refactoring

--- Projects/classical_planning/sym_logic.py
+++ Projects/classical_planning/sym_logic.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import List
+from typing import Tuple
 
 
 class SymLogicParser(object):
@@ -22,15 +22,16 @@ class SymLogicParser(object):
             elif statement.find('V') != -1:
                 column[key] = p_bool or q_bool
             elif statement.find(' =>') != -1:
-                first, second = statement[0], statement[5]
-                bool_index1, bool_index2 = self.index_order[first], self.index_order[second]
-                if key[bool_index1] and not key[bool_index2]:
-                    column[key] = False
-                else:
-                    column[key] = True
+                column[key] = self.get_implies_value(statement, key)
             elif statement.find('<=>') != -1:
                 column[key] = p_bool == q_bool
             else:
                 raise Exception('Do not know what to do with ' + statement)
         return column
 
+    def get_implies_value(self, sub_statement: str, input_key: Tuple[bool, bool]):
+        first, second = sub_statement[0], sub_statement[5]
+        bool_index1, bool_index2 = self.index_order[first], self.index_order[second]
+        if input_key[bool_index1] and not input_key[bool_index2]:
+            return False
+        return True
