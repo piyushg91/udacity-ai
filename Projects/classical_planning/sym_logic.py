b4fd72e3188c4dca90045fc59ef509d3782b8631
Piyush Gupta <piyushg91@gmail.com>
2020-06-08 22:14:19 -0700

    Handling not case

--- Projects/classical_planning/sym_logic.py
+++ Projects/classical_planning/sym_logic.py
@@ -5,9 +5,27 @@ from typing import List
 class SymLogicParser(object):
     def __init__(self):
         self.syms = ['p', 'q']
+        self.index_order = {'p': 0, 'q': 1}
         possibilities = [True, False]
         self.pos = set(itertools.product(possibilities, repeat=len(self.syms)))
 
     def create_column(self, statement: str):
-        for p in self.pos:
-            continue
+        column = {}
+        for p_bool, q_bool in self.pos:
+            key = (p_bool, q_bool)
+            if statement.find('~') != -1:
+                to_nullify = statement[1]
+                index_order = self.index_order[to_nullify]
+                column[key] = not key[index_order]
+            elif statement.find('^') != -1:
+                column[key] = p_bool and q_bool
+            elif statement.find('V') != -1:
+                column[key] = p_bool or q_bool
+            elif statement.find(' =>') != -1:
+                pass
+            elif statement.find('<=>') != -1:
+                pass
+            else:
+                raise Exception('Do not know what to do with ' + statement)
+        return column
+
--- Projects/classical_planning/sym_logic_test.py
+++ Projects/classical_planning/sym_logic_test.py
@@ -4,24 +4,26 @@ from sym_logic import SymLogicParser
 
 class SymLogicTest(TestCase):
     def setUp(self):
-        self.parser1 = SymLogicParser()
+        # self.parser1 = SymLogicParser()
         self.parser2 = SymLogicParser()
         # self.parser3 = SymLogicParser(['A', 'B', 'C'])
 
     def test__init__(self):
-        correct1 = {(True,), (False,)}
+        # correct1 = {(True,), (False,)}
         correct2 = {(True, True), (True, False), (False, True), (False, False)}
         # correct3 = {(True, True, True), (True, True, False), (True, False, True), (True, False, False),
         #             (False, True, True), (False, True, False), (False, False, True), (False, False, False)}
-        self.assertEqual(correct1, self.parser1.pos)
+        # self.assertEqual(correct1, self.parser1.pos)
         self.assertEqual(correct2, self.parser2.pos)
         # self.assertEqual(correct3, self.parser3.pos)
 
     def test_not_p(self):
         inp = '~p'
-        correct = {{'p': True}: {False},
-                   {'p': False}: {True}}
-        seen = self.parser1.create_column(inp)
+        correct = {(True, True): False,
+                   (True, False): False,
+                   (False, True): True,
+                   (False, False): True}
+        seen = self.parser2.create_column(inp)
         self.assertEqual(correct, seen)
 
     def test_and(self):
